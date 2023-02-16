3f6ec52b8b1cd7cd3a0af2b7ca2257f3b4e2d618
ntmokoen021 <ntmokoen021@student.wethinkcode.co.za>
2023-02-16 13:51:22 +0200

    need to send data to the database.

---
+++ registration_Dynamic-web-project/src/main/java/net/java/registration/controller/EmployeeServlet.java
@@ -0,0 +1,74 @@
+package net.java.registration.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.java.registration.dao.EmployeeDao;
+import net.java.registration.model.Employee;
+
+@WebServlet("/register")
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	 
+	private EmployeeDao employeedao = new EmployeeDao();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/EmployeeRegister.jsp");
+		dispatcher.forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		String address = request.getParameter("address");
+		String contact = request.getParameter("contact");
+		
+		Employee employee = new Employee();
+		employee.setFirstName(firstName);
+		employee.setLastName(lastName);
+		employee.setUserName(userName);
+		employee.setPassword(password);
+		employee.setAddress(address);
+		employee.setContact(contact);
+		
+		
+		
+		try {
+			employeedao.registerEmployee(employee);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/employeeRegistered.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
